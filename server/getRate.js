f7d36dc504d8e259a190941f9298430d07aa9da1
Your Name <alla7gon@gmail.com>
2022-07-28 12:35:41 +0300

    api get, post/subscruibe

---
+++ server/getRate.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const URL_CRYP ='https://api.coingecko.com/api/v3/coins/markets?vs_currency=UAH&order=market_cap_desc&per_page=1&page=1&sparkline=false';
+
+export async function reqCurrencyAPI () {
+    let currency = null;
+    await axios.get(URL_CRYP).then(res => {
+        currency = res.data[0].current_price
+    })
+        .catch(error => {
+            console.log(error);
+        });
+
+    return currency;
+}
--- server/index.js
+++ server/index.js
@@ -1,37 +1,41 @@
-//-----------------------save fs-----------------------------------
-var fs = require('fs');
-const data = 'db.json'
+//----------------------------------------------------------
+import fs from 'fs';
+const data = 'db.json';
 
 function getDatabase() {
-    const database = fs.readFileSync('db.json')
-    const data = JSON.parse(database)
-    console.log(data, 'data')
-    return data
+    const database = fs.readFileSync('db.json');
+    const data = JSON.parse(database);
+    console.log(data, 'data');
+    return data;
 }
 
 function addNewEmailToDB(email){
-    const db= getDatabase()
-    db.push(email)
-    fs.writeFileSync(data, JSON.stringify(db))
+    const db= getDatabase();
+    db.push(email);
+    fs.writeFileSync(data, JSON.stringify(db));
 }
 function checkEmail(email){
-    return getDatabase().find(el => el.email === email)
+    return getDatabase().find(el => el.email === email);
 }
 
 //------------------server--------------------------------
-const express = require('express')
-const axios = require('axios');
-var cors = require('cors')
+import express from 'express';
+import cors from 'cors';
 const PORT = 8080;
-const URL_CRYP ='https://api.coingecko.com/api/v3/coins/markets?vs_currency=UAH&order=market_cap_desc&per_page=1&page=1&sparkline=false';
 const app = express();
-app.use(cors())
+import bodyParser from "body-parser";
+import {reqCurrencyAPI} from "./getRate.js";
+
+app.use(cors());
 app.use(express.json())
+// app.use(bodyParser.json());
 
 app.post('/subscribe', async (req, res) => {
     const item = req.body
+    console.log(item,'dddddd')
     const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
     const test = emailRegexp.test(item.email)
+    console.log(test,'test')
     if(checkEmail(item.email) === undefined && test === true){
         addNewEmailToDB(item)
         res.json({ status: 'OK. Email was added' })
@@ -40,29 +44,15 @@ app.post('/subscribe', async (req, res) => {
     }
 })
 
-app.get('/rate',async (req,res)=>{
-    let btsCurrent = console.log(await reqCurrencyAPI (),'s33333ss');
-    try{
-        res.status(200).json({ status: 'OK.' })
+app.get("/rate", async (req, res) => {
+    try {
+        let btsRate = await reqCurrencyAPI();
+        res.status(200).send({ btc: `${btsRate}` });
+    } catch (e) {
+        res.status(400).send({ status: "error", error: "Invalid status value" });
     }
-    catch (e){
-        res.status(401).json({status: 'error'})
-    }
-})
-
-async function reqCurrencyAPI () {
-    let currency = null;
-    const dataCurrency = await axios.get(URL_CRYP).then(res => {
-        currency = res.data[0].current_price
-    })
-        .catch(error => {
-            console.log(error);
-        });
-
-    return currency;
-}
-
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running at ${PORT}`)
+    console.log(`Server running at ${PORT}`);
 })
