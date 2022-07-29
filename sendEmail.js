b8ea61105d5d427e3ecad44e83f12078bf507eb3
Your Name <alla7gon@gmail.com>
2022-07-29 10:13:30 +0300

    fixed: status err message

--- sendEmail.js
+++ sendEmail.js
@@ -1,34 +1,38 @@
 import nodemailer from "nodemailer";
-import {getDatabase} from "./saveEmailToBD.js";
-import {reqCurrencyBTC} from "./getRate.js";
+import { getDatabase } from "./saveEmailToBD.js";
+import { reqCurrencyBTC } from "./getRate.js";
 
 export async function sendEmailSubscriptions() {
-    let rate = await reqCurrencyBTC()
-    const transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email", //
-        port: 587,
-        auth: {
-            user: 'daija.prosacco@ethereal.email',
-            pass: 'HN6MDuuATDqaxGzWsC'
-        },
-    });
+  let rate = await reqCurrencyBTC();
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email", //
+    port: 587,
+    auth: {
+      user: "daija.prosacco@ethereal.email",
+      pass: "HN6MDuuATDqaxGzWsC",
+    },
+  });
 
-    const users = await getDatabase();
-    for (let i = 0; i < users.length; i++) {
-        const user = users[i].email;
-        console.log('user',user)
-        await info(user);
-    }
+  const users = await getDatabase();
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i].email;
+    console.log("user", user);
+    await info(user);
+  }
 
-    async function info(user) {
-        await transporter.sendMail({
-            from: '"Btc rate" <daija.prosacco@ethereal.email>',
-            to: user, // Test email address
-            subject: "I know current BCT currency",
-            text: `BTC to UAH is ${rate}`,
-            html: `<strong>BTC to UAH is ${rate}</strong>`,
-        });
-        console.log("Message sent: %s", info.messageId);
+  async function info(user) {
+    let status;
+    try {
+      status = await transporter.sendMail({
+        from: '"Btc rate" <daija.prosacco@ethereal.email>',
+        to: user, // Test email address
+        subject: "I know current BCT currency",
+        text: `BTC to UAH is ${rate}`,
+        html: `<strong>BTC to UAH is ${rate}</strong>`,
+      });
+    } catch (err) {
+      console.log("Sending error: " + err.message);
     }
+    console.log("Message sent: %s", status && status.messageId);
+  }
 }
-
